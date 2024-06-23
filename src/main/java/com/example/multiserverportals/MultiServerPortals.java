301e71435f30a35d9a8f7aadef688b9c0a2f8fdc
appipinopi <appipinopi@gmail.com>
2024-06-23 21:46:49 +0900

    1.0.0

--- src/main/java/com/example/multiserverportals/MultiServerPortals.java
+++ src/main/java/com/example/multiserverportals/MultiServerPortals.java
@@ -1,6 +1,7 @@
 package com.example.multiserverportals;
 
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -9,63 +10,76 @@ public class MultiServerPortals extends JavaPlugin implements CommandExecutor {
 
     @Override
     public void onEnable() {
-        // プラグインの有効化時の処理
-        getLogger().info("MultiServerPortals has been enabled.");
+        // プラグインが有効になったときの処理
+        getLogger().info("MultiServerPortals has been enabled!");
+        
         // コマンドの登録
         getCommand("move").setExecutor(this);
         getCommand("sp").setExecutor(this);
+        getCommand("savarproperties").setExecutor(this);
         getCommand("cos").setExecutor(this);
+        getCommand("connectingservers").setExecutor(this);
     }
 
     @Override
     public void onDisable() {
-        // プラグインの無効化時の処理
-        getLogger().info("MultiServerPortals has been disabled.");
+        // プラグインが無効になったときの処理
+        getLogger().info("MultiServerPortals has been disabled!");
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // コマンド実行時の処理
-        if (command.getName().equalsIgnoreCase("move")) {
-            return handleMoveCommand(sender, args);
-        } else if (command.getName().equalsIgnoreCase("sp")) {
-            return handleSpCommand(sender, args);
-        } else if (command.getName().equalsIgnoreCase("cos")) {
-            return handleCosCommand(sender, args);
-        }
-        return false;
-    }
-
-    private boolean handleMoveCommand(CommandSender sender, String[] args) {
-        // /move コマンドの処理
         if (!(sender instanceof Player)) {
-            sender.sendMessage("Only players can execute this command.");
+            sender.sendMessage("このコマンドはプレイヤーのみ実行可能です。");
             return true;
         }
 
         Player player = (Player) sender;
 
-        if (args.length < 1) {
-            player.sendMessage("Usage: /move <server name>");
+        if (command.getName().equalsIgnoreCase("move")) {
+            if (args.length < 2) {
+                player.sendMessage("Usage: /move <server name> <username>");
+                return true;
+            }
+            String serverName = args[0];
+            String username = args[1];
+
+            // TODO: 指定されたサーバーにプレイヤーを移動する処理を実装する
+
+            player.sendMessage("プレイヤーを " + serverName + " に移動しました。");
             return true;
         }
 
-        String serverName = args[0];
+        if (command.getName().equalsIgnoreCase("sp") || command.getName().equalsIgnoreCase("savarproperties")) {
+            if (args.length < 3) {
+                player.sendMessage("Usage: /sp <server name> <i|item|x|xp|ar|armor|e|enderchest|g|go> <true|false>");
+                return true;
+            }
+            String serverName = args[0];
+            String option = args[1];
+            boolean enable = Boolean.parseBoolean(args[2]);
 
-        // プレイヤーを指定したサーバーに移動する処理を実装する
+            // TODO: 特定のサーバーのアイテム、XP、防具、エンダーチェストの内容の転送を有効または無効にする処理を実装する
 
-        return true;
-    }
+            player.sendMessage("サーバー " + serverName + " の " + option + " を " + (enable ? "有効" : "無効") + " にしました。");
+            return true;
+        }
 
-    private boolean handleSpCommand(CommandSender sender, String[] args) {
-        // /sp コマンドの処理
-        // サーバー名、オプション、値を取得して設定を変更する処理を実装する
-        return true;
-    }
+        if (command.getName().equalsIgnoreCase("cos") || command.getName().equalsIgnoreCase("connectingservers")) {
+            if (args.length < 3) {
+                player.sendMessage("Usage: /cos <IP address> <port> <server name>");
+                return true;
+            }
+            String ipAddress = args[0];
+            int port = Integer.parseInt(args[1]);
+            String serverName = args[2];
 
-    private boolean handleCosCommand(CommandSender sender, String[] args) {
-        // /cos コマンドの処理
-        // 新しいサーバーを登録する処理を実装する
-        return true;
+            // TODO: 新しいサーバーを登録する処理を実装する
+
+            player.sendMessage("新しいサーバー " + serverName + " を登録しました。");
+            return true;
+        }
+
+        return false;
     }
 }
