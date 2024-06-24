c1e125bfc57e3cd3b4c5604744cd3212c36b2075
appipinopi <appipinopi@gmail.com>
2024-06-25 07:27:56 +0900

    1.0.0

--- src/main/java/com/example/multiserverportals/SpigotMain.java
+++ src/main/java/com/example/multiserverportals/SpigotMain.java
@@ -4,24 +4,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.example.multiserverportals.command.CosCommandHandler;
 import com.example.multiserverportals.command.MoveCommandHandler;
 import com.example.multiserverportals.command.SpCommandHandler;
+import com.example.multiserverportals.command.WhitelistCommandHandler;
 
 public class SpigotMain extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic specific to Spigot
+        // Plugin startup logic
         saveDefaultConfig(); // Load config.yml if it doesn't exist
         getLogger().info("MultiServerPortals for Spigot has been enabled.");
         getCommand("move").setExecutor(new MoveCommandHandler(this));
         getCommand("sp").setExecutor(new SpCommandHandler(this));
         getCommand("cos").setExecutor(new CosCommandHandler(this));
+        getCommand("wh").setExecutor(new WhitelistCommandHandler(this));
+        getCommand("whitelist").setExecutor(new WhitelistCommandHandler(this));
         getCommand("savarproperties").setExecutor(new SpCommandHandler(this)); // Alias for /sp
         getCommand("connectingservers").setExecutor(new CosCommandHandler(this)); // Alias for /cos
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic specific to Spigot
+        // Plugin shutdown logic
         getLogger().info("MultiServerPortals for Spigot has been disabled.");
     }
 }
