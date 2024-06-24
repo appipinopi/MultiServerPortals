34e8faa50ff84387e6eeeb7ee461d1b1e78e104a
appipinopi <appipinopi@gmail.com>
2024-06-25 07:15:32 +0900

    1.0.0

--- src/main/java/com/example/multiserverportals/PaperMain.java
+++ src/main/java/com/example/multiserverportals/PaperMain.java
@@ -1,32 +1,27 @@
 package com.example.multiserverportals;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import com.example.multiserverportals.command.CosCommandHandler;
+import com.example.multiserverportals.command.MoveCommandHandler;
+import com.example.multiserverportals.command.SpCommandHandler;
 
 public class PaperMain extends JavaPlugin {
 
-    private static PaperMain instance;
-
     @Override
     public void onEnable() {
-        instance = this;
+        // Plugin startup logic specific to PaperMC
+        saveDefaultConfig(); // Load config.yml if it doesn't exist
         getLogger().info("MultiServerPortals for PaperMC has been enabled.");
-
-        // Register command handlers
         getCommand("move").setExecutor(new MoveCommandHandler(this));
         getCommand("sp").setExecutor(new SpCommandHandler(this));
         getCommand("cos").setExecutor(new CosCommandHandler(this));
-
-        // Any additional setup or initialization
-        // Example: register event listeners
-        // getServer().getPluginManager().registerEvents(new MyListener(), this);
+        getCommand("savarproperties").setExecutor(new SpCommandHandler(this)); // Alias for /sp
+        getCommand("connectingservers").setExecutor(new CosCommandHandler(this)); // Alias for /cos
     }
 
     @Override
     public void onDisable() {
+        // Plugin shutdown logic specific to PaperMC
         getLogger().info("MultiServerPortals for PaperMC has been disabled.");
-    }
-
-    public static PaperMain getInstance() {
-        return instance;
     }
 }
