8d6a0c312885c8b975458a14a16966b80a753e54
appipinopi <appipinopi@gmail.com>
2024-06-25 07:15:54 +0900

    1.0.0

--- src/main/java/com/example/multiserverportals/SpigotMain.java
+++ src/main/java/com/example/multiserverportals/SpigotMain.java
@@ -1,31 +1,27 @@
 package com.example.multiserverportals;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import com.example.multiserverportals.command.CosCommandHandler;
+import com.example.multiserverportals.command.MoveCommandHandler;
+import com.example.multiserverportals.command.SpCommandHandler;
 
 public class SpigotMain extends JavaPlugin {
 
-    private static SpigotMain instance;
-
     @Override
     public void onEnable() {
-        instance = this;
-
-        // Register commands
-        getCommand("move").setExecutor(new MoveCommandHandler(instance));
-        getCommand("sp").setExecutor(new SpCommandHandler(instance));
-        getCommand("cos").setExecutor(new CosCommandHandler(instance));
-
-        // Plugin startup logic
-        getLogger().info("MultiServerPortals has been enabled.");
+        // Plugin startup logic specific to Spigot
+        saveDefaultConfig(); // Load config.yml if it doesn't exist
+        getLogger().info("MultiServerPortals for Spigot has been enabled.");
+        getCommand("move").setExecutor(new MoveCommandHandler(this));
+        getCommand("sp").setExecutor(new SpCommandHandler(this));
+        getCommand("cos").setExecutor(new CosCommandHandler(this));
+        getCommand("savarproperties").setExecutor(new SpCommandHandler(this)); // Alias for /sp
+        getCommand("connectingservers").setExecutor(new CosCommandHandler(this)); // Alias for /cos
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
-        getLogger().info("MultiServerPortals has been disabled.");
-    }
-
-    public static SpigotMain getInstance() {
-        return instance;
+        // Plugin shutdown logic specific to Spigot
+        getLogger().info("MultiServerPortals for Spigot has been disabled.");
     }
 }
