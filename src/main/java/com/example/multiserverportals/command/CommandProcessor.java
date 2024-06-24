23b48a47000a4c05df38a5a8852d90ab80310fe9
appipinopi <appipinopi@gmail.com>
2024-06-25 07:12:45 +0900

    1.0.0

--- src/main/java/com/example/multiserverportals/command/CommandProcessor.java
+++ src/main/java/com/example/multiserverportals/command/CommandProcessor.java
@@ -4,14 +4,33 @@ import com.example.multiserverportals.MultiServerPortals;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-public abstract class CommandProcessor implements CommandExecutor {
-    protected final MultiServerPortals plugin;
+public class MoveCommandHandler implements CommandExecutor {
+    private final MultiServerPortals plugin;
 
-    public CommandProcessor(MultiServerPortals plugin) {
+    public MoveCommandHandler(MultiServerPortals plugin) {
         this.plugin = plugin;
     }
 
     @Override
-    public abstract boolean onCommand(CommandSender sender, Command command, String label, String[] args);
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command can only be run by a player.");
+            return true;
+        }
+
+        if (args.length < 2) {
+            return false;
+        }
+
+        String serverName = args[0];
+        String playerName = args[1];
+
+        // プレイヤーを指定されたサーバーに移動するロジックをここに記述
+        sender.sendMessage(plugin.getConfig().getString("default-settings.move-message")
+                .replace("%player%", playerName)
+                .replace("%server%", serverName));
+        return true;
+    }
 }
