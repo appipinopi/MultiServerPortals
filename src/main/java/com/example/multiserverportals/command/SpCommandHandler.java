37eb55ae63f212049b1befc23d569d53205262dd
appipinopi <appipinopi@gmail.com>
2024-06-25 07:16:56 +0900

    1.0.0

--- src/main/java/com/example/multiserverportals/command/SpCommandHandler.java
+++ src/main/java/com/example/multiserverportals/command/SpCommandHandler.java
@@ -2,14 +2,12 @@ package com.example.multiserverportals.command;
 
 import com.example.multiserverportals.MultiServerPortals;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class SpCommandHandler implements CommandExecutor {
-    private final MultiServerPortals plugin;
+public class SpCommandHandler extends CommandProcessor {
 
     public SpCommandHandler(MultiServerPortals plugin) {
-        this.plugin = plugin;
+        super(plugin);
     }
 
     @Override
