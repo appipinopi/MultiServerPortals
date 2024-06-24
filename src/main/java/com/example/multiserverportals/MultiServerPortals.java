8f112f253990d7268283eb4f2663da72172a412b
appipinopi <appipinopi@gmail.com>
2024-06-25 07:11:46 +0900

    1.0.0

--- src/main/java/com/example/multiserverportals/MultiServerPortals.java
+++ src/main/java/com/example/multiserverportals/MultiServerPortals.java
@@ -1,21 +1,22 @@
 package com.example.multiserverportals;
 
+import org.bukkit.plugin.java.JavaPlugin;
 import com.example.multiserverportals.command.CosCommandHandler;
 import com.example.multiserverportals.command.MoveCommandHandler;
 import com.example.multiserverportals.command.SpCommandHandler;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class MultiServerPortals extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        saveDefaultConfig(); // Load config.yml if it doesn't exist
         getLogger().info("MultiServerPortals has been enabled.");
         getCommand("move").setExecutor(new MoveCommandHandler(this));
         getCommand("sp").setExecutor(new SpCommandHandler(this));
-        getCommand("savarproperties").setExecutor(new SpCommandHandler(this));
         getCommand("cos").setExecutor(new CosCommandHandler(this));
-        getCommand("connectingservers").setExecutor(new CosCommandHandler(this));
+        getCommand("savarproperties").setExecutor(new SpCommandHandler(this)); // Alias for /sp
+        getCommand("connectingservers").setExecutor(new CosCommandHandler(this)); // Alias for /cos
     }
 
     @Override
