af1188641da4aa2aa4f9531a5a475140bca8545d
appipinopi <appipinopi@gmail.com>
2024-06-25 07:16:35 +0900

    1.0.0

--- src/main/java/com/example/multiserverportals/command/MoveCommandHandler.java
+++ src/main/java/com/example/multiserverportals/command/MoveCommandHandler.java
@@ -2,15 +2,13 @@ package com.example.multiserverportals.command;
 
 import com.example.multiserverportals.MultiServerPortals;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class MoveCommandHandler implements CommandExecutor {
-    private final MultiServerPortals plugin;
+public class MoveCommandHandler extends CommandProcessor {
 
     public MoveCommandHandler(MultiServerPortals plugin) {
-        this.plugin = plugin;
+        super(plugin);
     }
 
     @Override
