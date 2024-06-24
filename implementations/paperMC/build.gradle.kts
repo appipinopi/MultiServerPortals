ac284e18874e6a002bfbcde9928ec85b0320828e
appipinopi <appipinopi@gmail.com>
2024-06-24 17:40:16 +0900

    1.0.0  # PaperMCビルド設定ファイル

--- implementations/paperMC/build.gradle.kts
+++ implementations/paperMC/build.gradle.kts
@@ -2,22 +2,70 @@ plugins {
     id("java")
 }
 
-group = "com.example"
-version = "1.0.0"
+group = "com.example.multiserverportals.paperMC"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
+val minecraftVersions = listOf(
+    "1.8",
+    "1.8.3",
+    "1.8.4",
+    "1.8.5",
+    "1.8.6",
+    "1.8.7",
+    "1.8.8",
+    "1.9",
+    "1.9.2",
+    "1.9.4",
+    "1.10",
+    "1.10.2",
+    "1.11",
+    "1.11.1",
+    "1.11.2",
+    "1.12",
+    "1.12.1",
+    "1.12.2",
+    "1.13",
+    "1.13.1",
+    "1.13.2",
+    "1.14",
+    "1.14.1",
+    "1.14.2",
+    "1.14.3",
+    "1.14.4",
+    "1.15",
+    "1.15.1",
+    "1.15.2",
+    "1.16.1",
+    "1.16.2",
+    "1.16.3",
+    "1.16.4",
+    "1.16.5",
+    "1.17",
+    "1.17.1",
+    "1.18",
+    "1.18.1",
+    "1.18.2",
+    "1.19",
+    "1.19.1",
+    "1.19.2",
+    "1.19.3",
+    "1.19.4",
+    "1.20",
+    "1.20.1",
+    "1.20.2",
+    "1.20.3",
+    "1.20.4",
+    "1.20.5",
+    "1.20.6",
+    "1.21"
+)
+
 dependencies {
-    compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
-}
-
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
-}
-
-tasks.jar {
-    archiveBaseName.set("MultiServerPortals")
-    archiveVersion.set("1.0.0")
+    minecraftVersions.forEach { version ->
+        implementation("com.destroystokyo.paper:paper-api:${version}-R0.1-SNAPSHOT")
+    }
 }
