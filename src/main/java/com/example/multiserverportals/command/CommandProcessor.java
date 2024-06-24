b7a40f4a6628a89ae05dcd13568534826177fd53
appipinopi <appipinopi@gmail.com>
2024-06-24 21:03:05 +0900

    1.0.0

---
+++ src/main/java/com/example/multiserverportals/command/CommandProcessor.java
@@ -0,0 +1,17 @@
+package com.example.multiserverportals.command;
+
+import com.example.multiserverportals.MultiServerPortals;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public abstract class CommandProcessor implements CommandExecutor {
+    protected final MultiServerPortals plugin;
+
+    public CommandProcessor(MultiServerPortals plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public abstract boolean onCommand(CommandSender sender, Command command, String label, String[] args);
+}
