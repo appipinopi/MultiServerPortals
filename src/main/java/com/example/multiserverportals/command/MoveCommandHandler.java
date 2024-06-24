1784f741b2a968714502ecba04e0b891ad81f386
appipinopi <appipinopi@gmail.com>
2024-06-25 07:13:31 +0900

    1.0.0

--- src/main/java/com/example/multiserverportals/command/MoveCommandHandler.java
+++ src/main/java/com/example/multiserverportals/command/MoveCommandHandler.java
@@ -2,34 +2,35 @@ package com.example.multiserverportals.command;
 
 import com.example.multiserverportals.MultiServerPortals;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class MoveCommandHandler extends CommandProcessor {
+public class MoveCommandHandler implements CommandExecutor {
+    private final MultiServerPortals plugin;
 
     public MoveCommandHandler(MultiServerPortals plugin) {
-        super(plugin);
+        this.plugin = plugin;
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command can only be run by a player.");
+            return true;
+        }
+
         if (args.length < 2) {
-            sender.sendMessage("Usage: /move <server name> <username>");
             return false;
         }
 
         String serverName = args[0];
         String playerName = args[1];
-        
-        Player player = plugin.getServer().getPlayer(playerName);
-        if (player != null) {
-            // Move player to the specified server logic
-            // This requires integration with BungeeCord
-            sender.sendMessage("Moving player " + playerName + " to server " + serverName);
-        } else {
-            sender.sendMessage("Player " + playerName + " not found.");
-        }
 
+        // プレイヤーを指定されたサーバーに移動するロジックをここに記述
+        sender.sendMessage(plugin.getConfig().getString("default-settings.move-message")
+                .replace("%player%", playerName)
+                .replace("%server%", serverName));
         return true;
     }
 }
