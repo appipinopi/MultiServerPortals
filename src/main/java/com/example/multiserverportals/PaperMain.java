77ddcbb9944b05623bf3b9621f1ffdfb756aa372
appipinopi <appipinopi@gmail.com>
2024-06-25 14:17:20 +0900

    1.0.0

--- src/main/java/com/example/multiserverportals/PaperMain.java
+++ src/main/java/com/example/multiserverportals/PaperMain.java
@@ -10,21 +10,22 @@ public class PaperMain extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        saveDefaultConfig(); // Load config.yml if it doesn't exist
+        // プラグイン起動ロジック
+        saveDefaultConfig(); // config.ymlが存在しない場合はロードする
         getLogger().info("MultiServerPortals for PaperMC has been enabled.");
         getCommand("move").setExecutor(new MoveCommandHandler(this));
         getCommand("sp").setExecutor(new SpCommandHandler(this));
         getCommand("cos").setExecutor(new CosCommandHandler(this));
         getCommand("wh").setExecutor(new WhitelistCommandHandler(this));
         getCommand("whitelist").setExecutor(new WhitelistCommandHandler(this));
-        getCommand("savarproperties").setExecutor(new SpCommandHandler(this)); // Alias for /sp
-        getCommand("connectingservers").setExecutor(new CosCommandHandler(this)); // Alias for /cos
+        getCommand("savarproperties").setExecutor(new SpCommandHandler(this)); // /spのエイリアス
+        getCommand("connectingservers").setExecutor(new CosCommandHandler(this)); // /cosのエイリアス
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        // プラグイン停止ロジック
         getLogger().info("MultiServerPortals for PaperMC has been disabled.");
     }
 }
+
