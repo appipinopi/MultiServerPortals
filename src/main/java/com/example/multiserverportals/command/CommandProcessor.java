b9c2daebef71b8f749a9d49a5f6aa0fc4bb5e8ff
appipinopi <appipinopi@gmail.com>
2024-06-25 13:56:53 +0900

    1.0.0

--- src/main/java/com/example/multiserverportals/command/CommandProcessor.java
+++ src/main/java/com/example/multiserverportals/command/CommandProcessor.java
@@ -1,17 +1,22 @@
 package com.example.multiserverportals.command;
 
-import com.example.multiserverportals.MultiServerPortals;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public abstract class CommandProcessor implements CommandExecutor {
-    protected MultiServerPortals plugin;
-
-    public CommandProcessor(MultiServerPortals plugin) {
-        this.plugin = plugin;
-    }
 
     @Override
-    public abstract boolean onCommand(CommandSender sender, Command command, String label, String[] args);
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command can only be run by a player.");
+            return false;
+        }
+        
+        Player player = (Player) sender;
+        return handleCommand(player, args);
+    }
+    
+    protected abstract boolean handleCommand(Player player, String[] args);
 }
